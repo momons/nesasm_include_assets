c5a1e2d63e174f5858c8e73db6aaee910165e390
Kazunari HARA <momonsnikuq@gmail.com>
2022-05-31 22:14:21 +0900

    fix: 軽微な修正

--- pallet/pallet.asm
+++ pallet/pallet.asm
@@ -9,14 +9,14 @@ memMapPalletSp = $3f10; ; スプライトパレットテーブルアドレス
 
 ; 指定アドレスのパレットをロードする
 ; @param 1 memMapPalletBg or memMapPalletSp
-; @param 2 パレットデータアドレス
+; @param 2 パレットデータアドレス(必ず16個)
 loadPallet .MACRO
     lda #(\1 / $100)
     sta $2006
     lda #(\1 % $100)
     sta $2006
-    ldx #$00
-    ldy #$10
+    ldx #0
+    ldy #16
 .COPY_PALLET\@:
     lda \2, x
     sta $2007
