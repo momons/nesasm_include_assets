8b43092e5cc0728da2a60d4160a73e63ce0c28df
Kazunari HARA <momonsnikuq@gmail.com>
2022-05-31 22:08:05 +0900

    feat: カラーパレットの設置を追加

---
+++ pallet/pallet.asm
@@ -0,0 +1,26 @@
+; ------------------------
+; カラーパレットの設定
+; ------------------------
+
+; メモリマップ
+memMapPalletAdd = $2006 ; パレットをロードするアドレスを指定
+memMapPalletBg = $3f00; ; BGパレットテーブルアドレス
+memMapPalletSp = $3f10; ; スプライトパレットテーブルアドレス
+
+; 指定アドレスのパレットをロードする
+; @param 1 memMapPalletBg or memMapPalletSp
+; @param 2 パレットデータアドレス
+loadPallet .MACRO
+    lda #(\1 / $100)
+    sta $2006
+    lda #(\1 % $100)
+    sta $2006
+    ldx #$00
+    ldy #$10
+.COPY_PALLET\@:
+    lda \2, x
+    sta $2007
+    inx
+    dey
+    bne .COPY_PALLET\@
+    .ENDM
