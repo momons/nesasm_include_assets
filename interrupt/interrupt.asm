b4dabf4a9e5dbd83ad929791bb0ac8690a9a7386
Kazunari HARA <momonsnikuq@gmail.com>
2022-06-01 22:23:31 +0900

    feat: 割り込み追加

---
+++ interrupt/interrupt.asm
@@ -0,0 +1,12 @@
+; ------------------------
+; 割り込み
+; ------------------------
+
+; リセット割り込み
+setResetInterrupt .MACRO
+    .bank bankInterrupt
+    .org memMapInterrupt
+    .dw 0
+    .dw RESET
+    .dw 0
+    .ENDM
