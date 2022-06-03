4ac1bd845a70b33c0805ec30c08566350d35f5c8
Kazunari HARA <momonsnikuq@gmail.com>
2022-06-03 20:53:25 +0900

    feat: DPCM制御を追加

---
+++ audio/audio_dpcm.asm
@@ -0,0 +1,20 @@
+; ------------------------
+; DPCM制御
+; ※動作未確認
+; ------------------------
+
+; ノイズ制御の設定
+; @param 1 サンプルレートビット数(0〜7)
+; @param 2 ボリューム(0〜255)
+; @param 3 音声テーブルのアドレス(Nx$40+$c000)
+; @param 4 テーブルバイトサイズ(Nx16+1)
+setAudioDpcm .MACRO
+    lda \1
+    sta memMapDpcmCtl1
+    lda \2
+    sta memMapDpcmCtl2
+    lda \3
+    sta memMapDpcmCtl3
+    lda \4
+    sta memMapDpcmCtl4
+    .ENDM
